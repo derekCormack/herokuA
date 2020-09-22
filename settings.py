cfaea9c9b5e1d1b5e5d873c8f6593dc7444d3248
[derekCormack] <[dcormack.dc@gmail.com]>
2020-09-22 14:46:33 -0600

    secret key

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import os
 
 SQLALCHEMY_DATABASE_URI =  os.environ.get('postgres://ywlpicmiptrres:f142823196b7ee397d5ba35381b8ece85fd8717e025420f3f338a16466730b9b@ec2-23-23-36-227.compute-1.amazonaws.com:5432/ddmh0o5t51m950')
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('f243tg24hg4545')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
