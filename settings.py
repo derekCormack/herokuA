ad2f5dbfc4248c4b40cf4b2363119e18d55468b6
[derekCormack] <[dcormack.dc@gmail.com]>
2020-10-08 15:47:04 -0600

    ...

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-import os
+# import os
 
-SQLALCHEMY_DATABASE_URI =  os.environ.get('')
-SECRET_KEY = os.environ.get('f243tg24hg4545')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI =  os.environ.get('')
+# SECRET_KEY = os.environ.get('f243tg24hg4545')
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
